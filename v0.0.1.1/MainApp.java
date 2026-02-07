310d26072d87d027068ff36e642ba8b49108c7d1
Lunasnow22 <daygod2019@gmail.com>
2026-02-08 00:56:26 +0700

    Refactor exit message to remove redundant text

--- v0.0.1.1/MainApp.java
+++ v0.0.1.1/MainApp.java
@@ -43,9 +43,7 @@ public class MainApp {
 
     private static void printExitMessage() {
         System.out.println("\n========================================");
-        System.out.println("    ขอบคุณที่ใช้งาน! สวัดดีวันจัน :) ufo มาssssss99999999999ssss");
-        System.out.println("    ขอบคุณที่ใช้งาน! สวัดดีวันจัน :) ufo มาssssssssss3333333333333333333");
-        System.out.println("    ขอบคุณที่ใช้งาน! สวัสดีวันจันทร์...ส่งไปเเล้วนะ :) ufo มา33หหหหหหหหหหหหหหหห33333333");
+        System.out.println("    ขอบคุณที่ใช้งาน! สวัดดีวันจัน :)");
         System.out.println("========================================");
     }
 }
