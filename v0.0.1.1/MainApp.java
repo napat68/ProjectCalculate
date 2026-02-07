6b99b5b180e1413bdb2da5e25e1216aa303b3c9f
Narin <naapii722@gmail.com>
2026-02-07 23:19:38 +0700

    Update exit message with new farewell text

--- v0.0.1.1/MainApp.java
+++ v0.0.1.1/MainApp.java
@@ -43,7 +43,7 @@ public class MainApp {
 
     private static void printExitMessage() {
         System.out.println("\n========================================");
-        System.out.println("    ขอบคุณที่ใช้งาน! เสร็จแล้วขอลาออกนะ :) ufo มา");
+        System.out.println("    ขอบคุณที่ใช้งาน! odogegiowbiwebuewg :) ufo มา");
         System.out.println("========================================");
     }
 }
