efaf36c90d924733a5310096f3490cd90cf39d2e
Lunasnow22 <daygod2019@gmail.com>
2026-02-07 21:28:26 +0700

    test

--- v0.0.1.1/MainApp.java
+++ v0.0.1.1/MainApp.java
@@ -43,7 +43,7 @@ public class MainApp {
 
     private static void printExitMessage() {
         System.out.println("\n========================================");
-        System.out.println("    ขอบคุณที่ใช้งาน! สวัสดีวันจันทร์... ");
+        System.out.println("    ขอบคุณที่ใช้งาน! สวัสดีวันจันทร์...สักวันไม่มีตังค์");
         System.out.println("========================================");
     }
 }
