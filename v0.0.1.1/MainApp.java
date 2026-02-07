b26493d885ea90e59aa3b76722ccf7564b78ffc1
Lunasnow22 <daygod2019@gmail.com>
2026-02-08 00:08:55 +0700

    Update exit message with new farewell text

--- v0.0.1.1/MainApp.java
+++ v0.0.1.1/MainApp.java
@@ -43,7 +43,7 @@ public class MainApp {
 
     private static void printExitMessage() {
         System.out.println("\n========================================");
-        System.out.println("    ขอบคุณที่ใช้งาน! สวัดดีวันจัน :) ufo มาssssssssss");
+        System.out.println("    ขอบคุณที่ใช้งาน! สวัดดีวันจัน :) ufo มาssssss222222222222222222222222ssss");
         System.out.println("========================================");
     }
 }
